1f82fd9fe32d4c19ef61915ecf6c5b10e377f668
Harshitha-git-hub <vyshnaviharshitha.konda@gmail.com>
2023-05-08 21:06:34 +0530

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/Harshitha-git-hub/goempproject/database"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+
+	database.ConnectDb()
+
+	app := fiber.New()
+
+	setupRoutes(app)
+
+	app.Listen(":3000")
+
+}
